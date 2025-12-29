5bb7d04aee27d26918f0d934711d532008de432c
Dany Christel <151681252+Danychr1@users.noreply.github.com>
2025-12-28 18:58:04 -0800

    Add STIG remediation PowerShell script template
    This PowerShell script is designed to remediate STIG-ID vulnerabilities by setting specific registry values. It includes metadata such as author, version, and tested systems.

---
+++ STIG_Remediation_Template.ps1
@@ -0,0 +1,34 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the associated STIG-ID vulnerability is remediated.
+
+.NOTES
+    Author          : Dany Christel
+    LinkedIn        : 
+    GitHub          : https://github.com/Danychr1
+    Date Created    : 12/28/2025
+    Last Modified   : 12/28/2025
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : 
+
+.DESCRIPTION
+    This script sets the following registry value:
+
+
+    This enforces:
+
+
+.TESTED ON
+    Date(s) Tested  : 12/29/2025
+    Tested By       : Dany Christel
+    Systems Tested  : Windows 11
+    PowerShell Ver. : 5.1.26100.6899
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\stig_remediation_template(STIG-ID-).ps1
+#>
+
+# YOUR CODE GOES HERE
