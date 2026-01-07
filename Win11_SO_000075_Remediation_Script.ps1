e02e818ad602f1e7eb380fd6b1cc83ba72ba2055
Dany Christel <151681252+Danychr1@users.noreply.github.com>
2026-01-06 18:26:26 -0800

    Add remediation script for STIG WN11-SO-000075
    This PowerShell script remediates the STIG-ID WN11-SO-000075 by disabling the 'Always install with elevated privileges' feature and Microsoft Consumer Experiences.

---
+++ Win11_SO_000075_Remediation_Script.ps1
@@ -0,0 +1,37 @@
+<#
+ .SYNOPSIS
+    This PowerShell script ensures that the associated STIG-ID (WN11-SO-000075) vulnerability is remediated installes feature 'Always install with elevated privileges' must be disabled.
+
+.NOTES
+    Author          : Dany Christel
+    LinkedIn        : https://www.linkedin.com/in/dany-christel/
+    GitHub          : https://github.com/Danychr1
+    Date Created    : 1/05/2026
+    Last Modified   : 1/06/2026
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-SO-000075
+
+
+.TITLE
+   Turn off Microsoft Consumer Experiences
+
+.DESCRIPTION
+   Configures the system to disable Microsoft Consumer Experiences (apps, promotions, etc.)
+  in compliance with DISA Windows 11 STIG WN11-SO-000075.
+
+    This enforces:
+
+
+.TESTED ON
+    Date(s) Tested  : 01/06/2026
+    Tested By       : Dany Christel
+    Systems Tested  : Windows 11
+    PowerShell Ver. : 5.1.26100.7462
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\Win11_SO_000075_Remediation_Script.ps1
+#>
