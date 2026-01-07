db94998b9ddcee6e1ea2c100561da6b575755042
Dany Christel <151681252+Danychr1@users.noreply.github.com>
2026-01-06 18:39:24 -0800

    Add remediation script for STIG WN11-UR-000070
    This PowerShell script remediates the STIG-ID WN11-UR-000070 by disabling the 'Always install with elevated privileges' feature and configuring the required legal notice before console logon.

---
+++ Win11_UR_000070_Remediation_Script.ps1
@@ -0,0 +1,37 @@
+<#
+ .SYNOPSIS
+    This PowerShell script ensures that the associated STIG-ID (WN11-UR-000070) vulnerability is remediated installes feature 'Always install with elevated privileges' must be disabled.
+
+.NOTES
+    Author          : Dany Christel
+    LinkedIn        : https://www.linkedin.com/in/dany-christel/
+    GitHub          : https://github.com/Danychr1
+    Date Created    : 1/05/2026
+    Last Modified   : 1/06/2026
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-UR-000070
+
+
+.TITLE
+   Configure required legal notice before console logon
+
+.DESCRIPTION
+   Sets the legal notice caption and text that appear before Windows logon, 
+in compliance with DISA Windows 11 STIG WN11-SO-000075.
+
+    This enforces:
+
+
+.TESTED ON
+    Date(s) Tested  : 01/06/2026
+    Tested By       : Dany Christel
+    Systems Tested  : Windows 11
+    PowerShell Ver. : 5.1.26100.7462
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\Win11_UR_000070_Remediation_Script.ps1
+#>
