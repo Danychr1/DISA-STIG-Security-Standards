0176e070fecc9f467c517a1dd49f038e791a7a0c
Dany Christel <151681252+Danychr1@users.noreply.github.com>
2026-01-06 18:14:00 -0800

    Create Win11_CC_000197_Remediation_Script.ps1

---
+++ Win11_CC_000197_Remediation_Script.ps1
@@ -0,0 +1,37 @@
+<#
+ .SYNOPSIS
+    This PowerShell script ensures that the associated STIG-ID (WN11-CC-000197) vulnerability is remediated installes feature 'Always install with elevated privileges' must be disabled.
+
+.NOTES
+    Author          : Dany Christel
+    LinkedIn        : https://www.linkedin.com/in/dany-christel/
+    GitHub          : https://github.com/Danychr1
+    Date Created    : 1/05/2026
+    Last Modified   : 1/06/2026
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000197
+
+
+.TITLE
+   Prevent Printing over HTTP
+
+.DESCRIPTION
+   Configures the system to prevent printing over HTTP, in compliance with
+   DISA Windows 11 STIG WN11-CC-000110.
+
+    This enforces:
+
+
+.TESTED ON
+    Date(s) Tested  : 01/06/2026
+    Tested By       : Dany Christel
+    Systems Tested  : Windows 11
+    PowerShell Ver. : 5.1.26100.7462
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\Win11_CC_000197_Remediation_Script.ps1
+#>
